72a441dedebd79866b9c9492ebeaaa30febe81c9
Lukas Vitala <lukas.vitala@seznam.cz>
2024-05-07 14:49:55 +0200

    Added register new user via API test, env variables used in separate file

--- tests/example-czechitas.spec.ts
+++ tests/example-czechitas.spec.ts
@@ -1,8 +1,13 @@
-import { test, expect } from "@playwright/test";
+import { expect, test } from "@playwright/test";
 import { AllPages } from "../pages";
+import { UserApi, createRandomUsername } from "../api-helpers/user-api";
 
-test.describe("Example test class for functionality showcase", () => {
+test.describe("Ukazkova test suita ve ktere najdes ruzne zpusoby jak a co je mozne v appce testovat", () => {
   let pages: AllPages;
+  let validLoginUsername = process.env.VALID_LOGIN_USERNAME as string;
+  let validLoginPassword = process.env.VALID_LOGIN_PASSWORD as string;
+  let validRegistrationUsername = process.env.VALID_REGISTRATION_USERNAME as string;
+  let validRegistrationPassword = process.env.VALID_REGISTRATION_PASSWORD as string;
 
   test.beforeEach(async ({ page }) => {
     pages = new AllPages(page);
@@ -12,8 +17,20 @@ test.describe("Example test class for functionality showcase", () => {
 
   test("successfulLoginTest", async () => {
     await pages.loginPage.visit();
-    await pages.loginPage.login("vojta.cerveny@czechitas.cz", "Hello123456");
+    await pages.loginPage.login(validLoginUsername, validLoginPassword);
     await pages.loginPage.navigationBar.expectUserIsLogged();
+  });
+
+  test("Register new user via API", async ({ page }) => {
+    // given
+    const userApi = new UserApi(page);
+    const username = createRandomUsername(3);
+
+    // when
+    const response = await userApi.registerUser(username, username);    
+    
+    // then
+    expect(response.status()).toBe(201);
   });
 
   // Parameterized test - using test.describe for parameterization
